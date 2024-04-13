2f5c2d356b25b6e13e7a786094538c9feba82414
ARTUR BERNARDO SILVA REIS <67850791+arturberna@users.noreply.github.com>
2024-04-13 08:59:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+// exemplo de variáveis numéricas
+let x = 5;
+let y = 10;
+console.log(x+y);
+//  exemplo com strings
+let texto = 'bem vindo ao javascript ';
+let nome = 'fulano';
+console.log(texto+nome);
